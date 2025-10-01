2a3c3ffdaee15b82c0dd4c4141835b617ff49305
ayman <aymanehab2002@gmail.com>
2025-10-01 19:10:36 +0300

    Controller Integeration tests

---
+++ Resturants.Api/Resturants.Api.Tests/Controllers/ResturantsControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using Resturants.Api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using FluentAssertions;
+
+namespace Resturants.Api.Controllers.Tests
+{
+    public class ResturantsControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        public ResturantsControllerTests()
+        {
+            _factory = new WebApplicationFactory<Program>();
+        }
+ 
+        [Fact()]
+        public async Task GetAll_ForValidRequest_Returns200Ok()
+        {
+            //arrange
+            var client = _factory.CreateClient();
+            //act 
+            var result = await client.GetAsync("/api/resturants?pageNumber=1&pageSize=10");
+            //assert
+            result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+        [Fact()]
+        public async Task GetAll_ForIncalidRequest_Returns400BadRequest()
+        {
+            //arrange
+            var client = _factory.CreateClient();
+            //act 
+            var result = await client.GetAsync("/api/resturants");
+            //assert
+            result.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+    }
+}
--- Resturants.Api/Resturants.Api/Program.cs
+++ Resturants.Api/Resturants.Api/Program.cs
@@ -1,62 +1,57 @@
-
-using Resturants.Infrastructure.Persistence;
 using Resturants.Infrastructure.Extentions;
 using Resturants.Infrastructure.Seeders;
 using Resturants.Application.Extentions;
 using Serilog;
-using Serilog.Events;
-using Serilog.Formatting.Compact;
 using Resturants.Api.Middlewares;
 using Resturants.Domain.Entites;
-using Microsoft.OpenApi.Models;
 using Resturants.Api.Extentions;
-namespace Resturants.Api
+namespace Resturants.Api;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+        builder.Services.AddApplicationService();
+        builder.Services.AddInfrastructureServices(builder.Configuration);
+        builder.AddPresentations();
+
+        var app = builder.Build();
+        var Scoped = app.Services.CreateScope();
+        var Seeder = Scoped.ServiceProvider.GetRequiredService<IResturantSeeder>();
+        await Seeder.SeedAsync();
+
+        app.UseMiddleware<ErrorHandlingMiddleware>();
+        app.UseMiddleware<RequestTimeLoggingMiddleware>();
+
+        app.UseSerilogRequestLogging();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-   
-            builder.Services.AddApplicationService();
-            builder.Services.AddInfrastructureServices(builder.Configuration);
-            builder.AddPresentations();
-           
-            var app = builder.Build();
-            var Scoped = app.Services.CreateScope();
-            var Seeder = Scoped.ServiceProvider.GetRequiredService<IResturantSeeder>();
-            await Seeder.SeedAsync();
-
-            app.UseMiddleware<ErrorHandlingMiddleware>();
-            app.UseMiddleware<RequestTimeLoggingMiddleware>();
-
-            app.UseSerilogRequestLogging();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-            app.MapGroup("api/identity")
-                .WithTags("Identity")
-                .MapIdentityApi<User>();
-            //app.UseAuthentication();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+        app.MapGroup("api/identity")
+            .WithTags("Identity")
+            .MapIdentityApi<User>();
+        //app.UseAuthentication();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
+ 
